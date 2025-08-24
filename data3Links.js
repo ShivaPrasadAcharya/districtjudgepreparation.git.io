832c5d5a48178833acb64e5f1f9e3d0347857753
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-24 13:22:02 +0545

    Update data3Links.js

--- data3Links.js
+++ data3Links.js
@@ -1,6 +1,14 @@
 // data3Links.js - Links Dataset
 var data3Links = `ID,Categry,Subject,Remark,Link
 011,Nyayik Awaj,Content/topic,pdf,nyayikawajcontent.pdf
+01,dj ppt, civillaw, pdf, B A 1 civil law.pdf
+02,dj ppt, strategic plan, pdf, A E 2 Strategic Plan.pdf
+03,dj ppt, Nepalese legal system, pdf, A C 1 Nepalese_Legal_System for judges.pdf
+04,dj ppt, देवानी कानूनको अवधारणा, pdf, B A 1 देवानी कानूनको अवधारणा.pdf
+05,dj ppt, succession, pdf, B A 1 f succession.pdf
+06,dj ppt, g guardian, pdf, B B 1 g guardian.pdf
+07,dj ppt, j adoption, pdf, B B 1 j adoption.pdf
+08,dj ppt, करार, pdf, B B 2 करार सम्बन्धी कानूनी व्यबस्था पैरवी 11,8.pdf
 `;
 // Dataset metadata
 var data3LinksInfo = {
@@ -10,5 +18,6 @@ var data3LinksInfo = {
     columns: 5,
     primaryKey: "ID"
 };
+
 
 
