1b80e33d8913215daddbb117c07fc369310db6b5
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-08-24 21:19:25 +0545

    updated content

--- data3Links.js
+++ data3Links.js
@@ -4,16 +4,18 @@ var data3Links = `ID,Categry,Subject,Remark,Link
 01,dj ppt, civillaw, pdf, B A 1 civil law.pdf
 02,dj ppt, strategic plan, pdf, A E 2 Strategic Plan.pdf
 03,dj ppt, Nepalese legal system, pdf, A C 1 Nepalese_Legal_System for judges.pdf
-04,dj ppt, देवानी कानूनको अवधारणा, pdf, B A 1 देवानी कानूनको अवधारणा.pdf
-05,dj ppt, succession, pdf, B A 1 f succession.pdf
-06,dj ppt, g guardian, pdf, B B 1 g guardian.pdf
-07,dj ppt, j adoption, pdf, B B 1 j adoption.pdf
-08,dj ppt, करार, pdf, B B 2 करार सम्बन्धी कानूनी व्यबस्था पैरवी 11,8.pdf
+04,dj ppt, देवानी कानूनको अवधारणा,pdf, B A 1 civillaw.pdf
+05,dj ppt, succession, pdf,B B 1 f succession.pdf
+06,dj ppt, guardian, pdf,B B 1 g guardian.pdf
+07,dj ppt, adoption, pdf,B B 1 j adoption.pdf
+08,dj ppt, करार, pdf,B B 2contract.pdf
+09,dj ppt, Dwarikaman Joshi-dewani-karyabidhi, pdf, B D 1 Dwarikaman Joshi  p.p-dewani-karyabidi-kanun.pdf
+10,dj ppt, Dwarikaman Joshi-dewani-karyabidhi, pdf, B D 1-4 Dwarikaman Joshi  p.p-dewani-karyabidi-kanun.pdf
 `;
 // Dataset metadata
 var data3LinksInfo = {
-    name: "Notes",
-    description: "Akala Devi's Notes",
+    name: "Notes of DJ",
+    description: "DJ pdf files",
     emoji: "🔗",
     columns: 5,
     primaryKey: "ID"
