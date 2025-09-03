77d60cd504461a5fd6ce9f878c6cea369ff9c03a
ShivaPrasadAcharya <shivaprasadacharya000000000000@gmail.com>
2025-09-03 14:44:50 +0000

    ok ok

--- data2Images.js
+++ data2Images.js
@@ -1,8 +1,8 @@
 // data2Images.js - Images Dataset
-var data2Images = `ID,Title,Description,Image
-001, Akala Devi,अकलादेव-लामाचौर, akaladevi1.jpg
-002, Akala Devi,अकलादेव-लामाचौर, akaladevi2.jpg
-002, Akala Devi,अकलादेव-लामाचौर, akaladevi3.jpg
+var data2Images = `ID,Title,Description,ImageLink
+001, Akala Devi,अकलादेव-लामाचौर, IMAGE/akaladevi2.jpg
+002, Akala Devi,अकलादेव-लामाचौर, IMAGE/akaladevi3.jpg
+003, Web developer,शिवप्रसाद आचार्य, IMAGE/myphoto.jpg
 
 `;
 
